52b047f63094e16b68e96c76e55b48839202df2e
Dariusz Zajac <darekdmc@gmail.com>
2020-03-15 20:12:11 +0100

    b15.03.2020

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { Zoom } from 'react-slideshow-image';
+import img1 from "../img/elewacja1.jpg"
+import img2 from "../img/elewacja2.jpg"
+import img3 from "../img/elewacja3.jpg"
+import img4 from "../img/elewacja4.jpg"
+import img5 from "../img/elewacja5.jpg"
+import photo1 from "../img/Widok 1.jpeg"
+import photo2 from "../img/Widok 2.jpeg"
+import photo3 from "../img/Widok 3.jpeg"
+import photo4 from "../img/Widok 4.jpeg"
+import photo5 from "../img/Widok 5.jpeg"
+import photo6 from "../img/Widok 6.jpeg"
+import photo7 from "../img/Widok 7.jpeg"
+import photo8 from "../img/Widok 8.jpeg"
+import photo9 from "../img/Widok 9.jpeg"
 
+const images = [img1, img2, img3, img4, img5];
+const photos = [photo1, photo2, photo3, photo4, photo5, photo6,  photo7, photo8, photo9];
 const Main = ()=>{
     return (
         <main className="page-main">
@@ -24,8 +41,8 @@ const Main = ()=>{
                 <div className="row">
                     <div className="col-6-12">
                         <div className="box">
-                            <img src="images/photo2.jpeg" alt="home2" className="box-image"/>
-                            <h2 className="box-title">Przykładowa aranżacja wnętrza</h2>
+                            <Slideshowmini className="box-image"/>
+                            <h3 className="box-title">Przykładowa aranżacja wnętrza</h3>
                             <div className="box-content">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, dolores ipsum
                                 laboriosam mollitia nihil omnis possimus repellendus. Ab aut consequatur dignissimos
@@ -39,7 +56,23 @@ const Main = ()=>{
                     <div className="col-6-12">
                         <div className="box">
                             <img src="images/photo3.jpeg" alt="home3" className="box-image"/>
-                            <h2 className="box-title">Zamieszkaj w Tulcach przy ulicy Krótkiej</h2>
+                            <h4 className="box-title">Zamieszkaj w Tulcach przy ulicy Krótkiej</h4>
+                            <div className="box-content">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, dolores ipsum
+                                laboriosam mollitia nihil omnis possimus repellendus. Ab aut consequatur dignissimos
+                                dolores dolorum, expedita illo incidunt natus nulla officiis perferendis quae
+                                quibusdam, repellendus similique temporibus totam voluptate voluptatem? Aliquam
+                                dicta exercitationem fugit illo iure labore laboriosam nostrum qui sunt
+                                voluptatibus.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12-12">
+                        <div className="box1">
+                            <Slideshow className="box-image"/>
+                            <h5 className="box-title">Twój nowy dom od 360.000 zł</h5>
                             <div className="box-content">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, dolores ipsum
                                 laboriosam mollitia nihil omnis possimus repellendus. Ab aut consequatur dignissimos
@@ -56,4 +89,30 @@ const Main = ()=>{
     )
 };
 
+const zoomOutProperties = {
+    duration: 4000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    scale: 0.4,
+    arrows: true
+};
+const Slideshow = () => {
+    return (
+        <Zoom {...zoomOutProperties}>
+            {
+                images.map((each, index) => <img alt="slideshow" key={index} style={{width: "100%"}} src={each} />)
+            }
+        </Zoom>
+    )
+};
+const Slideshowmini = () => {
+    return (
+        <Zoom {...zoomOutProperties}>
+            {
+                photos.map((each, index) => <img alt="slideshow2" key={index} style={{width: "100%"}} src={each} />)
+            }
+        </Zoom>
+    )
+};
 export default Main
